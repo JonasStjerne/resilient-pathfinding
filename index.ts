b84fadfba7aca8e4ea7b7b9dd1927d25e32db27b
Jonas Stjerne <jost@trifork.com>
2023-10-16 10:35:14 +0200

    Remove m-ui

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import search from "./algo/AStarSearch.js";
 import algoInit from "./algo/index.js";
 import clientInit, { drawIds } from "./client/index.js";
-import '/node_modules/@material/web/button/filled-button.js';
-import '/node_modules/@material/web/button/outlined-button.js';
-import '/node_modules/@material/web/checkbox/checkbox.js';
 const grid = algoInit();
 clientInit(grid);
 
