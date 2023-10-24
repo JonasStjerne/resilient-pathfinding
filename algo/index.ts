dd0a989d82b54addd52aee2a42a120ed58e40feb
Jonas Stjerne <jost@trifork.com>
2023-10-24 09:51:08 +0200

    Remove commented out test

--- algo/index.ts
+++ algo/index.ts
@@ -3,13 +3,6 @@ import { computeMue } from "./mue.js";
 
 export default function algoInit() {
   makeGrid();
-  // grid[9][3].type = "water";
-  // grid[9][3].incomingDistEdges = [grid[9][4]];
-
-  // grid[9][4].distEdges = [new Edge(grid[9][3], 1)];
-  // grid[9][4].incomingDistEdges = [grid[9][5]];
-
-  // grid[9][5].distEdges = [new Edge(grid[9][4], 1)];
   computeMue(grid);
 //   test_funktion();
 }
