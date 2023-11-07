ee769f9827459f6edd1bfa4b859c5e55a9d536b5
Jonas Stjerne <jost@trifork.com>
2023-11-07 09:03:07 +0100

    We dont need this ;)

--- utils/telemetry.ts
+++ utils/telemetry.ts
@@ -1,6 +1,6 @@
 export function trackTime(func: Function) {
   const startTime = performance.now()
-  const result = func.apply(this)
+  const result = func.apply(null)
   const endTime = performance.now()
   console.log(`Function ${func.name} took ${endTime - startTime}ms to run.`)
   return result
