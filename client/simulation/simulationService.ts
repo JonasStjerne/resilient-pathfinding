5914ba2f256cca8e0986e331398872269ccd34a0
Jonas Stjerne <jost@trifork.com>
2023-11-13 15:46:04 +0100

    Run simulation

--- client/simulation/simulationService.ts
+++ client/simulation/simulationService.ts
@@ -1,4 +1,8 @@
+import search, { Position } from '../../algo/AStarSearch.js'
+import { results, simulateRoute } from '../../algo/Simulate.js'
 import { grid } from '../../algo/grid.js'
+import { Grid } from '../../algo/models/Grid.js'
+import { endNode } from '../index.js'
 import { SimulationOptions, Stats } from './models.js'
 
 export class simulationService {
@@ -9,7 +13,7 @@ export class simulationService {
     const riskFactor = Number((<HTMLInputElement>document.getElementById('risk-factor-sim')).value)
     const runCount = Number((<HTMLInputElement>document.getElementById('iteration-count')).value)
 
-    const maps = this.#getMap()
+    const maps = this.#getMaps()
 
     const options: SimulationOptions = {
       maps,
@@ -20,7 +24,7 @@ export class simulationService {
     return options
   }
 
-  static #getMap() {
+  static #getMaps() {
     //Todo: hook up to save service when working with file system
     return [grid]
   }
@@ -48,5 +52,31 @@ export class simulationService {
     const progressBar = <HTMLSpanElement>document.getElementById('sim-progress-bar')
     progressBar.hidden = done
     progressBar.style.width = `${percent.toString()}%`
+  }
+
+  static runSimulation() {
+    const options = this.getSimOptions()
+    const maps = this.#getMaps()
+    const simResults: results[] = []
+    for (let i = 0; i < options.runCount; i++) {
+      const startPos = this.#getStartOrEndPos(maps[i], 'start')
+      const endPos = this.#getStartOrEndPos(maps[i], 'end')
+      if (options.algoVersion == 'v0.2') {
+        const path = search(startPos!, endPos!, maps[i], options.riskFactor)
+        path.filter((nodeId) => nodeId)
+        const simResult = simulateRoute(maps[i], path as number[], startPos!, endNode!, search, 0.5, options.riskFactor)
+        simResults.push(simResult)
+      }
+    }
+  }
+
+  static #getStartOrEndPos(grid: Grid, type: 'start' | 'end'): Position | undefined {
+    for (let col = 0; col < grid.length; col++) {
+      for (let row = 0; row < grid.length; row++) {
+        if (grid[col][row].type == type) {
+          return { x: col, y: row }
+        }
+      }
+    }
   }
 }
