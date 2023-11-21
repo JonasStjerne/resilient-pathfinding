dd4be7819248ae69e71d941cdaae54b6416fa6da
Jonas Stjerne <jost@trifork.com>
2023-11-21 09:57:17 +0100

    Refactor simservice

--- client/simulation/simulationService.ts
+++ client/simulation/simulationService.ts
@@ -7,40 +7,6 @@ import { GridJSON, recreateNodeCircularReference } from '../save/saveService.js'
 import { SimulationOptions, Stats } from './models.js'
 
 export class simulationService {
-  static async getSimOptions() {
-    const algoVersion = <'v0.1' | 'v0.2'>(
-      (<HTMLInputElement>document.querySelector('input[name="algo-version"]:checked')).value
-    )
-    const riskFactor = Number((<HTMLInputElement>document.getElementById('risk-factor-sim')).value)
-    const runCount = Number((<HTMLInputElement>document.getElementById('iteration-count')).value)
-
-    const maps = <Grid[]>await this.#getMaps()
-
-    const options: SimulationOptions = {
-      maps,
-      runCount,
-      algoVersion,
-      riskFactor,
-    }
-    return options
-  }
-
-  static async #getMaps() {
-    const maps = await this.#getSaveFiles()
-    return maps
-  }
-
-  static #setStats(stats: Stats) {
-    this.#setStat('comp-time', `${stats.comptime.toFixed(1)}ms`)
-    this.#setStat('traveledDistance', `${stats.traveledDistance.toFixed(1)}m`)
-    this.#setStat('pushover', `${(stats.pushover * 100).toFixed(0)}%`)
-    this.#setStat('success-rate', `${(stats.successRate * 100).toFixed(0)}%`)
-  }
-
-  static #setStat(id: string, value: string) {
-    ;(<HTMLHeadElement>document.getElementById(id)).innerHTML = value
-  }
-
   static setLoadingState(percent: number, done: boolean) {
     const runningSpinner = <HTMLSpanElement>document.getElementById('sim-running-spinner')
     done ? runningSpinner.classList.add('d-none') : runningSpinner.classList.remove('d-none')
@@ -53,7 +19,7 @@ export class simulationService {
   }
 
   static async runSimulation() {
-    const options = await this.getSimOptions()
+    const options = await this.#getSimOptions()
     const statsByMap: Array<{ name: string; stats: Stats }> = []
     const globalStats: Stats = { comptime: 0, traveledDistance: 0, pushover: 0, successRate: 0 }
 
@@ -106,6 +72,40 @@ export class simulationService {
     const averageStats = this.#getAverageStats(options.runCount, options.maps.length, globalStats)
     this.#setStats(averageStats)
     return statsByMap
+  }
+
+  static async #getMaps() {
+    const maps = await this.#getSaveFiles()
+    return maps
+  }
+
+  static async #getSimOptions() {
+    const algoVersion = <'v0.1' | 'v0.2'>(
+      (<HTMLInputElement>document.querySelector('input[name="algo-version"]:checked')).value
+    )
+    const riskFactor = Number((<HTMLInputElement>document.getElementById('risk-factor-sim')).value)
+    const runCount = Number((<HTMLInputElement>document.getElementById('iteration-count')).value)
+
+    const maps = <Grid[]>await this.#getMaps()
+
+    const options: SimulationOptions = {
+      maps,
+      runCount,
+      algoVersion,
+      riskFactor,
+    }
+    return options
+  }
+
+  static #setStats(stats: Stats) {
+    this.#setStat('comp-time', `${stats.comptime.toFixed(1)}ms`)
+    this.#setStat('traveledDistance', `${stats.traveledDistance.toFixed(1)}m`)
+    this.#setStat('pushover', `${(stats.pushover * 100).toFixed(0)}%`)
+    this.#setStat('success-rate', `${(stats.successRate * 100).toFixed(0)}%`)
+  }
+
+  static #setStat(id: string, value: string) {
+    ;(<HTMLHeadElement>document.getElementById(id)).innerHTML = value
   }
 
   static #getStartOrEndPos(grid: Grid, type: 'start' | 'goal'): Position | undefined {
